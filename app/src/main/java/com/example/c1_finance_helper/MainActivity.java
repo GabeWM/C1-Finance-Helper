cc5ad0a2102133e4aa5401c7943a9ce3a1da2849
Mark Jung <markojungo@gmail.com>
2020-02-15 02:50:59 -0500

    IDK

--- app/src/main/java/com/example/c1_finance_helper/MainActivity.java
+++ app/src/main/java/com/example/c1_finance_helper/MainActivity.java
@@ -18,15 +18,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
---
+++ app/src/main/java/com/example/c1_finance_helper/util/NessieAPI.java
@@ -0,0 +1,12 @@
+package com.example.c1_finance_helper.util;
+
+import com.reimaginebanking.api.nessieandroidsdk.requestclients.NessieClient;
+
+public class NessieAPI {
+
+    public static void main(String[] args) {
+
+        NessieClient client = NessieClient.getInstance("9acc940321abe272c81d8329651cb93b");
+
+    }
+}

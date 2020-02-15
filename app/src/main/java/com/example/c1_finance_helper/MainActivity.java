5440b36521db7aa341ed0ebae61e26291c6f0ed9
Gabriel Margolis <Gabriel@Gabriels-MacBook-Pro.local>
2020-02-15 02:47:28 -0500

    line chart test

--- app/src/main/java/com/example/c1_finance_helper/MainActivity.java
+++ app/src/main/java/com/example/c1_finance_helper/MainActivity.java
@@ -1,13 +1,14 @@
 package com.example.c1_finance_helper;
 
-import lecho.lib.hellocharts.view.PieChartView;
+import lecho.lib.hellocharts.view.*;
 import lecho.lib.hellocharts.model.*;
 
 import android.os.Bundle;
-import java.util.*;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import java.util.*;
 
 import android.view.Menu;
 import android.view.MenuItem;
@@ -22,18 +23,40 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        PieChartView pieChartView = findViewById(R.id.chart);
-        List<SliceValue> pieData = new ArrayList<>();
-        pieData.add(new SliceValue(10, Color.RED).setLabel("Health"));
-        pieData.add(new SliceValue(30, Color.BLUE).setLabel("Housing"));
-        pieData.add(new SliceValue(20, Color.GREEN).setLabel("Food"));
-        pieData.add(new SliceValue(30, Color.MAGENTA).setLabel("Transportation"));
-        pieData.add(new SliceValue(5, Color.YELLOW).setLabel("Personal and Family"));
-        pieData.add(new SliceValue(5, Color.BLACK).setLabel("Other"));
-        PieChartData pieChartData = new PieChartData(pieData);
-        pieChartData.setHasLabels(true).setValueLabelTextSize(8);
-        pieChartView.setPieChartData(pieChartData);
+        LineChartView lineChartView = findViewById(R.id.chart);
+        String[] axisData = {"Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept",
+                "Oct", "Nov", "Dec"};
+        int[] yAxisData = {50, 20, 15, 30, 20, 60, 15, 40, 45, 10, 90, 18};
+        List yAxisValues = new ArrayList();
+        List axisValues = new ArrayList();
 
+        Line line = new Line(yAxisValues).setColor(Color.rgb(198,40,41));;
+        for(int i = 0; i < axisData.length; i++){
+            axisValues.add(i, new AxisValue(i).setLabel(axisData[i]));
+        }
+
+        for (int i = 0; i < yAxisData.length; i++){
+            yAxisValues.add(new PointValue(i, yAxisData[i]));
+        }
+        List lines = new ArrayList();
+        lines.add(line);
+        LineChartData data = new LineChartData();
+        data.setLines(lines);
+        lineChartView.setLineChartData(data);
+        Axis axis = new Axis();
+        axis.setTextSize(16);
+        axis.setValues(axisValues);
+        axis.setTextColor(Color.rgb(0,61,110));
+        data.setAxisXBottom(axis);
+        Axis yAxis = new Axis();
+        yAxis.setTextColor(Color.rgb(0,61,110));
+        yAxis.setTextSize(16);
+        yAxis.setName("Sales in millions");
+        data.setAxisYLeft(yAxis);
+        Viewport viewport = new Viewport(lineChartView.getMaximumViewport());
+        viewport.top =110;
+        lineChartView.setMaximumViewport(viewport);
+        lineChartView.setCurrentViewport(viewport);
     }
 
     @Override
